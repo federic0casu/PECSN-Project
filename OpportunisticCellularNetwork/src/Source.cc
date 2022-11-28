89c43fa25c402d00e6679bea9f1ab5c00d80ed8c
Daniel Deiana <d.deiana1@studenti.unipi.it>
2022-11-28 11:53:00 +0100

    Source module is working, need to add packet handler in "Antenna.cc"

--- OpportunisticCellularNetwork/src/Source.cc
+++ OpportunisticCellularNetwork/src/Source.cc
@@ -16,49 +16,53 @@
 #include "Source.h"
 #include "Packet_m.h"
 
-Define_Module(Source);
+
 
 #define DEBUG ;
 
+namespace opportunisticcellularnetwork {
 
-simtime_t Source::getDelay(bool isUniform) {
+    Define_Module(Source);
 
-    if (isUniform)
-        return uniform(0,par("maxDelay"));
-    else
-        return exponential((simtime_t)par("exponentialMean"));
+    simtime_t Source::getDelay(bool isUniform) {
+
+        if (isUniform)
+            return uniform(0,par("maxDelay"));
+        else
+            return exponential((simtime_t)par("exponentialMean"));
+    }
+
+
+    // When a source gets initialized it starts by setting a timer
+    void Source::initialize()
+    {
+        simtime_t delay = getDelay(par("isDelayUniform"));
+
+        scheduleAt(simTime() + delay, timerMessage);
+    }
+
+
+    void Source::handleMessage(cMessage *msg)
+    {
+        // When timer message arrives, a packet gets forwarded out
+        Packet *packet = new Packet("packet");
+
+        // generating packet size and timestamp
+        int size = intuniform(0,par("maxPacketSize"));
+        packet->setSize(size);
+        packet->setTimestamp(simTime());
+
+    #ifdef DEBUG
+        EV << "Packet with size : " << packet->getSize() << " sent at " << packet->getTimestamp() << endl;
+    #endif
+
+        // sending out the message
+        send(packet, "out");
+
+        // re-scheduling timer
+        simtime_t delay = getDelay(par("isDelayUniform"));
+        scheduleAt(simTime() + delay, msg);
+    }
+
 }
-
-
-// When a source gets initialized it starts by setting a timer
-void Source::initialize()
-{
-    simtime_t delay = getDelay(par("isDelayUniform"));
-
-    scheduleAt(simTime() + delay, timerMessage);
-}
-
-
-void Source::handleMessage(cMessage *msg)
-{
-    // When timer message arrives, a packet gets forwarded out
-    Packet *packet = new Packet("packet");
-
-    // generating packet size and timestamp
-    int size = intuniform(0,par("maxPacketSize"));
-    packet->setSize(size);
-    packet->setTimestamp(simTime());
-
-#ifdef DEBUG
-    EV << "Packet with size : " << packet->getSize() << " sent at " << packet->getTimestamp() << endl;
-#endif
-
-    // sending out the message
-    send(packet, "out");
-
-    // re-scheduling timer
-    simtime_t delay = getDelay(par("isDelayUniform"));
-    scheduleAt(simTime() + delay, msg);
-}
-
 
